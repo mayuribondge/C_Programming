521ff4301e4779bac50ad324fe7b4c30a677f160
mayuribondge <166983403+mayuribondge@users.noreply.github.com>
2025-11-03 15:56:03 +0530

    Add files via upload

---
+++ Assignment1.c/Program1_3.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+
+void Display()
+{
+
+    int iCnt=0;
+    iCnt=5;
+    while (iCnt>=1)
+    {
+        printf("%d" , iCnt);
+        iCnt++;
+    }
+    
+}
+
+int main()
+{
+    Display();
+    return 0;
+}
